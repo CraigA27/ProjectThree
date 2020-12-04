acb2a2fd04b65044fad024f3a98656ec62f47977
cam <campbellmurray0@gmail.com>
2020-12-04 14:06:04 +0000

    Initial Commit

---
+++ project3_server/src/test/java/com/example/codeclan/project3_server/Project3ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.project3_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
