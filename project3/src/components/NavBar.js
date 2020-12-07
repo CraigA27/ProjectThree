cac3e84d0d716d32042ece8fa0762c2c747f0892
cam <campbellmurray0@gmail.com>
2020-12-07 13:18:55 +0000

    nav css

--- project3/src/components/NavBar.js
+++ project3/src/components/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return(
         <header className="nav-container">
-            <h1>Nav</h1>
-            <img className="header-logo"/> 
-                <ul>
+            <div className="logo-container">
+                    <img src="https://png2.cleanpng.com/sh/dc599486ac1265438bd34bdee36ab0a6/L0KzQYm3UsAzN6VufZH0aYP2gLBuTgNvbZJwfeR8LYPrf7a0lvVkfJD3ReRAbn7sfri0kBhwbaQyTdMCNHTlSLOCVPJlOGgzUaoANEi3SIe4VcE4PmE8UKMENkC8QnB3jvc=/kisspng-sneakers-shoe-vector-running-shoes-5a74db8b94bd07.9854848615176078196092.png" className="header-logo"/>
+            </div>
+                <ul className="nav-links"> 
                     <li className="navLink">
                         <a href="/">Home</a>
                     </li>
