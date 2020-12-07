babc557d792003b9c85db2e925b29c1edb0c863d
cyrilishabiyi <ComeUp123>
2020-12-07 14:41:23 +0000

    Home page

--- project3/src/components/NavBar.js
+++ project3/src/components/NavBar.js
@@ -5,12 +5,9 @@ const NavBar = () => {
     return(
         <header className="nav-container">
             <div className="logo-container">
-                    <img src="https://png2.cleanpng.com/sh/dc599486ac1265438bd34bdee36ab0a6/L0KzQYm3UsAzN6VufZH0aYP2gLBuTgNvbZJwfeR8LYPrf7a0lvVkfJD3ReRAbn7sfri0kBhwbaQyTdMCNHTlSLOCVPJlOGgzUaoANEi3SIe4VcE4PmE8UKMENkC8QnB3jvc=/kisspng-sneakers-shoe-vector-running-shoes-5a74db8b94bd07.9854848615176078196092.png" className="header-logo"/>
+            <a href="/"><img src="https://png2.cleanpng.com/sh/dc599486ac1265438bd34bdee36ab0a6/L0KzQYm3UsAzN6VufZH0aYP2gLBuTgNvbZJwfeR8LYPrf7a0lvVkfJD3ReRAbn7sfri0kBhwbaQyTdMCNHTlSLOCVPJlOGgzUaoANEi3SIe4VcE4PmE8UKMENkC8QnB3jvc=/kisspng-sneakers-shoe-vector-running-shoes-5a74db8b94bd07.9854848615176078196092.png" className="header-logo"/></a>
             </div>
-                <ul className="nav-links"> 
-                    <li className="navLink">
-                        <a href="/">Home</a>
-                    </li>
+                <ul className="nav-links">  
                     <li className="navLink">
                         <a href="/customers">Sign In / Create Account</a>
                     </li>
