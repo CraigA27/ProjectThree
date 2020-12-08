fe933542cdace5788a06141cccef6cd7ef623e5b
cyrilishabiyi <ComeUp123>
2020-12-08 09:21:46 +0000

    features added to db

--- project3_server/src/main/java/com/example/codeclan/project3_server/Controller/OrderController.java
+++ project3_server/src/main/java/com/example/codeclan/project3_server/Controller/OrderController.java
@@ -2,16 +2,15 @@ package com.example.codeclan.project3_server.Controller;
 
 import com.example.codeclan.project3_server.Models.Customer;
 import com.example.codeclan.project3_server.Models.Order;
+import com.example.codeclan.project3_server.Models.Trainer;
 import com.example.codeclan.project3_server.Repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class OrderController {
@@ -24,9 +23,21 @@ public class OrderController {
         return new ResponseEntity<>(orderRepository.findAll(), HttpStatus.OK);
     }
 
+    @GetMapping(value = "/orders/{id}")
+    public ResponseEntity<Optional<Order>> getSpecificTrainer(@PathVariable Long id){
+        return new ResponseEntity<>(orderRepository.findById(id), HttpStatus.OK);
+    }
+
     @PostMapping(value = "/orders")
     public ResponseEntity<Order> postOrder(@RequestBody Order order){
         orderRepository.save(order);
         return new ResponseEntity<>(order, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/orders/{id}")
+    public ResponseEntity<Order> deleteOrder(@PathVariable Long id){
+        Order found = orderRepository.getOne(id);
+        orderRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
